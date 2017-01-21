fb6e7853c3f84babe014ee3110628352cb85b8fc
Ethan Tarr <ebt0640@yahoo.com>
2017-01-20 23:18:24 -0800

    Non Physics based wave system
    Ethan's wave system.

---
+++ GGJ/Assets/SquareBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareBehavior : MonoBehaviour {
+
+	private int Amplitude = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float FinalYPos = 0;
+		foreach (GameObject pulse in GameObject.FindGameObjectsWithTag("Pulse")) {
+			float xPos = transform.position.x;
+			float xPulsePos = pulse.transform.position.x;
+			if (xPos - xPulsePos < (Amplitude * 2) && xPos - xPulsePos > 0) {
+				FinalYPos += Amplitude - Mathf.Abs (xPos - (xPulsePos + Amplitude));
+				//transform.position = new Vector3 (xPos, Amplitude - Mathf.Abs (xPos - (xPulsePos + Amplitude)), 0);
+			} else if (xPos - xPulsePos < 0 && xPos - xPulsePos > (Amplitude * -2)) {
+				FinalYPos += -Amplitude + Mathf.Abs (xPos - (xPulsePos - Amplitude));
+				//transform.position = new Vector3 (xPos, -Amplitude + Mathf.Abs (xPos - (xPulsePos - Amplitude)), 0);
+			} else {
+				//transform.position = new Vector3 (xPos, 0, 0);
+			}
+
+		}
+		transform.position = new Vector3 (transform.position.x, FinalYPos, 0);
+	}
+}
---
+++ GGJ/Assets/WaveGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGenerator : MonoBehaviour {
+	public GameObject pulse;
+	public GameObject antiPulse;
+	public GameObject WavePulse;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("t")) {
+			Instantiate (pulse, new Vector3(-10,0,0), Quaternion.identity);
+			//Instantiate (antiPulse, new Vector3(-2,0,0), Quaternion.identity);
+		}
+	}
+
+}
+
+
+
